039e6e4f2d941169d0892aacf9d8f1617176186a
dhiyaul10 <144310130+dhiyaul10@users.noreply.github.com>
2023-11-09 16:42:47 +0700

    Add files via upload

--- TugasIndividu4.java
+++ TugasIndividu4.java
@@ -1,30 +1,52 @@
+import java.util.Scanner;
+
 public class TugasIndividu4 {
     public static void main(String[] args) {
+        Scanner input08 = new Scanner(System.in);
 
-        String[][] atletPerCabang = {
-            {"Annisa", "Ayu", "Anya", "Anggi", "Asri"},
-            {"Budi", "Bidi", "Badu", "Bada", "Bili"},
-            {"Caca", "Cici", "Cece", "Cintiya", "Cessa"},
-            {"Deni", "Dani", "Danu", "Doni", "Desta"}
-        };
+        String[] cabangOlahraga = {"badminton", "tenis meja", "basket", "bola voly"};
+        String[][] dataAtlet = new String[4][5];
 
-        for (int i = 0; i < atletPerCabang.length; i++) {
-
-            for (int j = 0; j < atletPerCabang[i].length - 1; j++) {
-                for (int k = j + 1; k < atletPerCabang[i].length; k++) {
-                    if (atletPerCabang[i][j].compareTo(atletPerCabang[i][k]) > 0) {
-                        String temp = atletPerCabang[i][j];
-                        atletPerCabang[i][j] = atletPerCabang[i][k];
-                        atletPerCabang[i][k] = temp;
-                    }
-                }
-            }
-
-            System.out.println("Nama Atlet Cabang ke-" + (i + 1) + " (ascending):");
-            for (int j = 0; j < atletPerCabang[i].length; j++) {
-                System.out.println(atletPerCabang[i][j]);
+        for (int i = 0; i < cabangOlahraga.length; i++) {
+            System.out.println("Masukkan nama atlet untuk cabang " + cabangOlahraga[i]);
+            for (int j = 0; j < dataAtlet[i].length; j++) {
+                System.out.print("Atlet " + (j + 1) + ": ");
+                dataAtlet[i][j] = input08.nextLine();
             }
             System.out.println();
         }
+
+        System.out.println("Data Atlet sebelum diurutkan:");
+        for (int i = 0; i < cabangOlahraga.length; i++) {
+            System.out.print("Cabang " + cabangOlahraga[i] + ": ");
+            for (int j = 0; j < dataAtlet[i].length; j++) {
+                System.out.print(dataAtlet[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < cabangOlahraga.length; i++) {
+            for (int j = 0; j < dataAtlet[i].length - 1; j++) {
+                for (int k = 0; k < dataAtlet[i].length - 1 - j; k++) {
+                    if (dataAtlet[i][k].compareTo(dataAtlet[i][k + 1]) > 0) {
+
+                        String temp = dataAtlet[i][k];
+                        dataAtlet[i][k] = dataAtlet[i][k + 1];
+                        dataAtlet[i][k + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        System.out.println("\nData Atlet setelah diurutkan secara ascending:");
+        for (int i = 0; i < cabangOlahraga.length; i++) {
+            System.out.print("Cabang " + cabangOlahraga[i] + ": ");
+            for (int j = 0; j < dataAtlet[i].length; j++) {
+                System.out.print(dataAtlet[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        input08.close();
     }
 }
